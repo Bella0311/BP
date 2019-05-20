51f25b57c2639227137496a52ee1839b69771e58
Isabella Lütken <bella@isabellas-macbook-pro.local>
2019-05-20 16:02:45 +0200

    Initial Commit

---
+++ MyCoach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCoach
+//
+//  Created by Isabella Anna Maria Lütken on 20/05/2019.
+//  Copyright © 2019 Isabella Anna Maria Lütken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
